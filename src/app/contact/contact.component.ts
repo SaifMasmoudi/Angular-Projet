99418a367bd0a5c6b335ffcc1be014db914dfadb
SaifMasmoudi <saifmasmoudi7@gmail.com>
2024-05-31 23:26:48 +0100

    version 23:26

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,14 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Email } from 'src/Modeles/Email';
 import { BoiteReceptionService } from 'src/Services/boite-reception.service';
+
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.css']
 })
-export class ContactComponent  {
+export class ContactComponent implements OnInit {
   email: Email | null = null;
   emailForm!: FormGroup;
   idcourant!: string;
@@ -21,26 +22,25 @@ export class ContactComponent  {
 
   ngOnInit(): void {
     this.idcourant = this.activatedRoute.snapshot.params['id'];
-      this.initForm();
+    this.initForm();
   }
 
-
   onSubmit(): void {
+    if (this.emailForm.valid) {
       const emailToSave = this.emailForm.value;
       this.emailService.sendEmail(emailToSave).subscribe(() => {
         this.resetForm();
-          alert('Votre message a été envoyé avec succès !');
-        });
+        alert('Votre message a été envoyé avec succès !');
+      });
+    } else {
+      alert('Veuillez remplir tous les champs correctement.');
+    }
   }
-
-
 
   resetForm() {
     this.emailForm.reset();
   }
 
-
-  
   initForm(): void {
     this.emailForm = new FormGroup({
       name: new FormControl(null, [Validators.required]),
@@ -49,7 +49,4 @@ export class ContactComponent  {
       message: new FormControl(null, [Validators.required])
     });
   }
-
-
-  
 }
